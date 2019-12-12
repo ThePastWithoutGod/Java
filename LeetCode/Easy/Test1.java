e3be84ebf800846bb1b8abbbf3521a5e06b61b21
HouKe <33999841+ThePastWithoutGod@users.noreply.github.com>
2019-12-12 17:54:57 +0800

    两数之和
    给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
    你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。

---
+++ LeetCode/Easy/Test1.java
@@ -0,0 +1,21 @@
+package leetcode.NumArry;
+
+public class Test1 {
+    public int[] twoSum(int[] nums, int target)
+    {
+        for(int i = 0; i < nums.length; i++)
+        {
+            for(int j = i+1; j < nums.length; j++)
+            {
+                if(nums[i] == target - nums[j])
+                {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        /**
+         * 表示向方法传递了一个不合法的参数
+         */
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
